5d96c4694ed973abd658b5bac0fba685cb8733ea
Nilansh Tribhuvan <reachnilansh@gmail.com>
2026-08-08 13:26:28 +0530

    Time: 72 ms (28.28%), Space: 73.3 MB (89.79%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,40 +1,14 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        int n=nums.length;
-        for(int i=(k-2)/2;i>=0;i--){
-             heapify(nums,i,k);
+        PriorityQueue<Integer>p= new PriorityQueue<>();
+        for(int num: nums){
+            p.add(num);
+        
+        if(p.size()>k){
+            p.poll();
         }
-        for(int i=k;i<nums.length;i++){
-            if(nums[i]>nums[0]){
-                int temp= nums[i];
-                nums[i]=nums[0];
-                nums[0]=temp;
-                heapify(nums,0,k);
-            }
-            
         }
-       return nums[0];
+        return p.peek();
         
     }
-    public void heapify(int[] nums,int i,int n){
-        
-        int smallest = i;
-        int left =2*i+1;
-        int right = 2*i+2;
-
-        if(left<n && nums[smallest]>nums[left]){
-            smallest=left;
-        }
-        if(right<n && nums[smallest]>nums[right]){
-            smallest=right;
-        }
-        if(smallest!=i){
-            int temp=nums[smallest];
-            nums[smallest]=nums[i];
-            nums[i]=temp;
-
-        heapify(nums,smallest,n) ;
-        }
-    }
-
 }
