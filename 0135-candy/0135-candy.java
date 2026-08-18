41a4f26ad9ae413c6a99bc01161594dddafca0e8
Nilansh Tribhuvan <reachnilansh@gmail.com>
2026-08-18 11:44:21 +0530

    Time: 3 ms (86.47%), Space: 52.6 MB (5.25%) - LeetHub

--- 0135-candy/0135-candy.java
+++ 0135-candy/0135-candy.java
@@ -2,9 +2,9 @@ class Solution {
     public int candy(int[] ratings) {
         int n=ratings.length;
         int[] left=new int[n];
-        int[] right=new int[n];
+        
         left[0]=1;
-        right[n-1]=1;
+       
         for(int i=1;i<n;i++){
             if(ratings[i]>ratings[i-1]){
                 left[i]=left[i-1]+1;
@@ -12,16 +12,15 @@ class Solution {
                 left[i]=1;
             }
         }
+        int sum=left[n-1];
+        int right=1;
         for(int i=n-2;i>=0;i--){
             if(ratings[i]>ratings[i+1]){
-                right[i]=right[i+1]+1;
+                right=right+1;
             }else{
-                right[i]=1;
+              right=1;
             }
-        }
-        int sum=0;
-        for(int i=0;i<n;i++){
-            sum+=Math.max(left[i],right[i]);
+            sum+=Math.max(left[i],right);
         }
         return sum;
     }
