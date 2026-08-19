32560240b5343f4ec3cca93a0894d421432f39f1
Nilansh Tribhuvan <reachnilansh@gmail.com>
2026-08-19 11:59:03 +0530

    Time: 1 ms (98.14%), Space: 46.9 MB (76.1%) - LeetHub

---
+++ 0057-insert-interval/0057-insert-interval.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        int n=intervals.length;
+        int i=0;
+        List<int[]> result=new ArrayList<>();
+        if(n==0){
+            result.add(newInterval);
+        }
+        while(i<n){
+            while(i<n && intervals[i][1]<newInterval[0]){
+                result.add(intervals[i]);
+                i++;
+            }
+            while(i<n && intervals[i][0]<=newInterval[1]){
+                newInterval[0]=Math.min(intervals[i][0],newInterval[0]);
+                newInterval[1]=Math.max(intervals[i][1],newInterval[1]);
+                i++;
+            }
+            result.add(newInterval);
+            while(i<n){
+                result.add(intervals[i]);
+                i++;
+            }
+        }
+        return result.toArray(new int[result.size()][]);
+
+    }
+}
