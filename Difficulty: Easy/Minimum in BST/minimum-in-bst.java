acd66ead8fcef18303bf468b2be56fd5e10385f9
Nilansh Tribhuvan <reachnilansh@gmail.com>
2026-09-10 13:47:37 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Minimum in BST/minimum-in-bst.java
@@ -0,0 +1,26 @@
+/*
+Definition for Node
+class Node {
+    int data;
+    Node left;
+    Node right;
+    Node(int data) {
+        this.data = data;
+        left = right = null;
+    }
+}
+*/
+
+class Solution {
+    public int minValue(Node root) {
+        // code here
+        if(root.left==null){
+            return root.data;
+        }else{
+            while(root.left!=null){
+                root=root.left;
+            }
+        }
+        return root.data;
+    }
+}
